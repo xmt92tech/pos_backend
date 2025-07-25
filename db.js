b7412f75ed546b7b1f786dd91346a1cdc7ee9039
xmt92tech <ximingwork92@gmail.com>
2025-07-25 12:10:54 +0800

    update db port using .env

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host:  process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
