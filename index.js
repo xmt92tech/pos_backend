33724f8b70fb88b666236617c13192e12894cf1c
xmt92tech <ximingwork92@gmail.com>
2025-07-25 16:55:16 +0800

    update the cors url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://pos-app-xm92techs-projects.vercel.app',
   credentials: true
 }))
 
